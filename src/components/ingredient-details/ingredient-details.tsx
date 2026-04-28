e9065d0d5082306c0a6b43ebc536509dd4117eae
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-04-28 17:35:39 +0300

    fix: correct getError import in ingredient-details

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -4,13 +4,13 @@ import { IngredientDetailsUI } from '../ui/ingredient-details';
 import { useParams } from 'react-router-dom';
 import { useSelector } from '../../services/store';
 import { getIngredients, getIsLoading } from '../../services/ingredients-slice';
-import { getError } from '../../services/order-slice';
+import { getOrderError } from '../../services/order-slice';
 
 export const IngredientDetails: FC = () => {
   const { id } = useParams<{ id: string }>();
   const ingredients = useSelector(getIngredients);
   const isLoading = useSelector(getIsLoading);
-  const error = useSelector(getError);
+  const error = useSelector(getOrderError);
 
   const ingredientData = useMemo(
     () => ingredients.find((ingredient) => ingredient._id === id) ?? null,

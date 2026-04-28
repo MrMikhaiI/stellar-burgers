248efdb2939c87923375b5de41ead04ccd761838
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-04-28 17:28:02 +0300

    fix: app-header username, constructor-element handlers, ingredient-details loader

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,18 +1,26 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
-import { useSelector } from '../../services/store';
-import { getIngredients } from '../../services/ingredients-slice';
 import { useParams } from 'react-router-dom';
+import { useSelector } from '../../services/store';
+import { getIngredients, getIsLoading } from '../../services/ingredients-slice';
+import { getError } from '../../services/order-slice';
 
 export const IngredientDetails: FC = () => {
   const { id } = useParams<{ id: string }>();
   const ingredients = useSelector(getIngredients);
-  const ingredientData = ingredients.find((item) => item._id === id);
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
 
-  if (!ingredientData) {
+  const ingredientData = useMemo(
+    () => ingredients.find((ingredient) => ingredient._id === id) ?? null,
+    [id, ingredients]
+  );
+
+  if (!ingredientData || isLoading) {
     return <Preloader />;
   }
+  if (error) return <>error</>;
 
   return <IngredientDetailsUI ingredientData={ingredientData} />;
 };

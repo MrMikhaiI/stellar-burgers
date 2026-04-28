b74e7e72c367015f01ce476fd820ae6a8ffa6fad
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-04-28 18:08:10 +0300

    fix: add NavLink to app-header navigation

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -7,29 +7,31 @@ import {
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
+import { Link } from 'react-router-dom';
+import { PATHS } from '../../../utils/constants';
 
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
-        <>
+        <Link className={styles.header_link} to={PATHS.HOME}>
           <BurgerIcon type={'primary'} />
           <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </>
-        <>
+        </Link>
+        <Link className={styles.header_link} to={PATHS.FEED}>
           <ListIcon type={'primary'} />
           <p className='text text_type_main-default ml-2'>Лента заказов</p>
-        </>
+        </Link>
       </div>
-      <div className={styles.logo}>
+      <Link className={styles.logo} to={PATHS.HOME}>
         <Logo className='' />
-      </div>
-      <div className={styles.link_position_last}>
+      </Link>
+      <Link className={styles.link_position_last} to={PATHS.PROFILE}>
         <ProfileIcon type={'primary'} />
         <p className='text text_type_main-default ml-2'>
           {userName || 'Личный кабинет'}
         </p>
-      </div>
+      </Link>
     </nav>
   </header>
 );

b5059a39e5e60e2f98aa142d92de3ec8cfe39a57
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-04-28 17:09:14 +0300

    fix: connect feed-info, order-card and profile-menu to Redux store

--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,11 +1,26 @@
 import { FC } from 'react';
-import { useLocation } from 'react-router-dom';
-import { ProfileMenuUI } from '@ui';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Preloader, ProfileMenuUI } from '@ui';
+import { useDispatch, useSelector } from '../../services/store';
+import { getIsUserLoading, logoutUser } from '../../services/user-slice';
+import { unwrapResult } from '@reduxjs/toolkit';
+import { PATHS } from '../../utils/constants';
 
 export const ProfileMenu: FC = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsUserLoading);
+  const navigate = useNavigate();
   const { pathname } = useLocation();
 
-  const handleLogout = () => {};
+  const handleLogout = async () => {
+    try {
+      const result = await dispatch(logoutUser());
+      unwrapResult(result);
+      navigate(PATHS.LOGIN);
+    } catch {}
+  };
+
+  if (isLoading) return <Preloader />;
 
   return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
 };

6f7b89b2800cb6b41b62a43f9c7ab3c4f681474d
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-04-28 18:23:45 +0300

    fix: use NavLink for active link highlighting in app-header

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -7,34 +7,46 @@ import {
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { PATHS } from '../../../utils/constants';
 
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
-        <Link className={styles.link} to={PATHS.HOME}>
+        <NavLink
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
+          to={PATHS.HOME}
+        >
           <BurgerIcon type={'primary'} />
           <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </Link>
-        <Link className={styles.link} to={PATHS.FEED}>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
+          to={PATHS.FEED}
+        >
           <ListIcon type={'primary'} />
           <p className='text text_type_main-default ml-2'>Лента заказов</p>
-        </Link>
+        </NavLink>
       </div>
-      <Link className={styles.logo} to={PATHS.HOME}>
+      <NavLink className={styles.logo} to={PATHS.HOME}>
         <Logo className='' />
-      </Link>
-      <Link
-        className={`${styles.link} ${styles.link_position_last}`}
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          `${styles.link} ${styles.link_position_last} ${isActive ? styles.link_active : ''}`
+        }
         to={PATHS.PROFILE}
       >
         <ProfileIcon type={'primary'} />
         <p className='text text_type_main-default ml-2'>
           {userName || 'Личный кабинет'}
         </p>
-      </Link>
+      </NavLink>
     </nav>
   </header>
 );
